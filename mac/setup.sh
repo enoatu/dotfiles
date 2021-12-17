1fb6117bff550f531f2d2777041ebb923fafc6f8
enoatu <acro1452@gmail.com>
2021-12-17 10:23:58 +0900

    :wrench: delete unneed files

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 brew install git tmux gsed jq wget cmake gcc tree
+
+# Font + icon Install for mac
+git clone --branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh
+rm -fr nerd-fonts
+# select iTerm2 > profile > NotoSansMono Nerd Font
