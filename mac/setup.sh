323a6d87665f7954f9057d097ad26e52785505ad
enoatu <acro1452@gmail.com>
2022-01-08 17:31:15 +0900

    :wrench: add coc-fzf-preview

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-brew install git tmux gsed jq wget cmake gcc tree
+brew install git tmux gsed ggrep jq wget cmake gcc tree ripgrep
 
 # Font + icon Install for mac
 git clone --branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
