22760495d603f1b47ed4693f715a59389c6e9fb3
Atsushi ENOMOTO <enotiru@moove.co.jp>
2023-08-14 12:46:49 +0900

    modify init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -52,7 +52,7 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "<Leader>d", ":Gdiff<CR>:windo set wrap<CR>")
 -- "タブ、空白、改行の可視化
 vim.o.list = true
-vim.o.listchars = "tab:▸\\-,trail:◀,extends:>,precedes:<,nbsp:%"
+vim.o.listchars = "tab:▸▸,trail:◀,extends:>,precedes:<,nbsp:%"
 -- vim.o.listchars:append "eol:↴"
 -- "全角スペースをハイライト表示
 -- function! ZenkakuSpace()
@@ -126,6 +126,7 @@ local switchTabMessage = "4スペ"
 
 vim.cmd("set sw=4") -- shiftwidth
 vim.cmd("set ts=4") -- tabstop
+vim.keymap.set('n', '<C-i>', ':lua SwitchIndent()<CR>', { noremap = true })
 vim.keymap.set('n', '<C-o>', ':lua SwitchIndent()<CR>', { noremap = true })
 function SwitchIndent()
   if switchTab == 1 then
@@ -236,14 +237,98 @@ require("lazy").setup({
       vim.keymap.set("n", "<C-al>", ":AnyJumpLastResults<CR>")
     end,
   },
+  -- vim.ui.selectなどを装飾する
   {
-    "lewis6991/gitsigns.nvim",
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  {
+    "enoatu/gitsigns.nvim",
+    -- dir = "~/MyDevelopment/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup {
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          changedelete = { text = '≃' },
+        },
+        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+        current_line_blame = true,
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          -- Actions
+          map('n', '<leader>hs', gs.stage_hunk)
+          map('n', '<leader>hr', gs.reset_hunk)
+          map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+          map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      }
     end,
-  }
+  },
 })
 
+function ChangeBranchForComparison(branch)
+  vim.g.gitsigns_head = branch
+  package.loaded.gitsigns.refresh()
+end
+
+vim.g.gitsigns_head = ""
+vim.keymap.set("n", "m", ":lua SwitchGutter()<CR>", { noremap = true })
+local switchGutterBranches = { "", "origin/main", "origin/dev", "origin/master", "origin/staging", "main", "dev", "master", "staging" }
+local isSwitchGutter = false
+function SwitchGutter()
+  if isSwitchGutter then
+    return
+  end
+  isSwitchGutter = true
+  vim.notify("or :ChangeBranchForComparison(branch)", vim.log.levels.INFO)
+  vim.ui.select(switchGutterBranches, {
+    prompt = "Select the branch for comparison",
+   }, function(item, lnum)
+    if item and lnum then
+      ChangeBranchForComparison(item)
+      vim.notify(string.format("selected '%s' (idx %d)", item, lnum), vim.log.levels.INFO)
+    else
+      vim.notify("Selection canceled", vim.log.levels.INFO)
+    end
+  isSwitchGutter = false
+  end)
+end
 
 -- " 確定キーをTABからC-lに変更
 -- copilot for nvim
