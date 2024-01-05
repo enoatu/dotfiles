762ff9fe60852463044f283d1177dccd771ed240
Atsushi ENOMOTO <enotiru@moove.co.jp>
2024-01-05 22:12:33 +0900

    install.sh update

--- install.sh
+++ install.sh
@@ -316,7 +316,7 @@ install_rg() {
       url='https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz'
       TMPDIR=$(mktemp -d)
       cd $TMPDIR
-      wget -O - $url | tar zxf - --strip-component=1
+      curl -L -o - $url | tar zxf - --strip-component=1
       mv rg ${DOTFILES}/installs/
     )
   fi
