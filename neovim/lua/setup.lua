391f7c07878169f6a2732901bbf9074b13b0392b
enoatu <acro1452@gmail.com>
2024-12-07 18:29:52 +0900

    neovimプラグインの追加
    - nvim-bqfプラグインの追加
    - fzfプラグインの追加
    - text-case.nvimプラグインの追加

--- neovim/lua/setup.lua
+++ neovim/lua/setup.lua
@@ -200,6 +200,41 @@ require("lazy").setup({
             end,
         },
         {
+            'kevinhwang91/nvim-bqf',
+            ft = 'qf'
+        },
+        {
+            'junegunn/fzf',
+            run = function()
+                vim.fn['fzf#install']()
+            end
+        },
+        {
+            -- スネークケースとか変える
+            "johmsalas/text-case.nvim",
+            dependencies = { "nvim-telescope/telescope.nvim" },
+            config = function()
+                require("textcase").setup({})
+                require("telescope").load_extension("textcase")
+            end,
+            keys = {
+                "ga", -- Default invocation prefix
+                { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+            },
+            cmd = {
+                -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+                "Subs",
+                "TextCaseOpenTelescope",
+                "TextCaseOpenTelescopeQuickChange",
+                "TextCaseOpenTelescopeLSPChange",
+                "TextCaseStartReplacingCommand",
+            },
+            -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+            -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+            -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+            lazy = false,
+        },
+        {
             "enoatu/backseat.nvim",
             config = function()
                 require("backseat").setup({
