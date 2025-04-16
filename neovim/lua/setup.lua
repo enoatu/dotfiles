993bb4d786ea7a89c6f33ddd472f140cdec71fbc
enoatu <acro1452@gmail.com>
2025-04-16 20:12:59 +0900

    :wrench: gitsigns設定

--- neovim/lua/setup.lua
+++ neovim/lua/setup.lua
@@ -72,7 +72,7 @@ require("lazy").setup({
                     -- current_line_blame = true,
                     attach_to_untracked = false,
                     on_attach = function(bufnr)
-                        local gs = package.loaded.gitsigns
+                        local gitsigns = package.loaded.gitsigns
                         local function map(mode, l, r, opts)
                             opts = opts or {}
                             opts.buffer = bufnr
@@ -84,7 +84,7 @@ require("lazy").setup({
                                 return "]c"
                             end
                             vim.schedule(function()
-                                gs.next_hunk()
+                                gitsigns.next_hunk()
                             end)
                             return "<Ignore>"
                         end, { expr = true, desc = "次のhunkへ移動" })
@@ -93,11 +93,48 @@ require("lazy").setup({
                                 return "[c"
                             end
                             vim.schedule(function()
-                                gs.prev_hunk()
+                                gitsigns.prev_hunk()
                             end)
                             return "<Ignore>"
                         end, { expr = true, desc = "前のhunkへ移動" })
                         -- 設定によっては行単位でstage することもできる
+                        -- Actions
+                        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "Stage hunk" })
+                        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "Reset hunk" })
+
+                        map('v', '<leader>hs', function()
+                          gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+                        end, { desc = "Stage hunk" })
+
+                        map('v', '<leader>hr', function()
+                          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+                        end, { desc = "Reset hunk" })
+
+                        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Stage buffer" })
+                        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Reset buffer" })
+                        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Preview hunk" })
+                        map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
+
+                        map('n', '<leader>hb', function()
+                          gitsigns.blame_line({ full = true })
+                        end, { desc = "Blame line" })
+
+                        map('n', '<leader>hd', gitsigns.diffthis, { desc = "Diff this" })
+
+                        map('n', '<leader>hD', function()
+                          gitsigns.diffthis('~')
+                        end, { desc = "Diff this ~" })
+
+                        map('n', '<leader>hQ', function() gitsigns.setqflist('all') end, { desc = "Set qflist" })
+                        map('n', '<leader>hq', gitsigns.setqflist, { desc = "Set qflist" })
+
+                        -- Toggles
+                        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "Toggle blame" })
+                        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "Toggle deleted" })
+                        map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = "Toggle word diff" })
+
+                        -- Text object
+                        map({'o', 'x'}, 'ih', gitsigns.select_hunk, { desc = "Select hunk" })
                     end,
                 })
                 -- require("scrollbar.handlers.gitsigns").setup()
