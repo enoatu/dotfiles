93857749284f5af8b944517915cdd8b807dd9885
enoatu <acro1452@gmail.com>
2025-08-01 10:26:14 +0900

    :wrench: modify enotiru

--- neovim/lua/setup.lua
+++ neovim/lua/setup.lua
@@ -12,6 +12,32 @@ local has_words_before = function()
     return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
+local ssh_config = {
+    ssh_binary = "sshm",
+    scp_binary = "sshm --scp",
+    ssh_config_file_paths = { "$HOME/.ssh/config" },
+    -- ssh_prompts = {
+    --     {
+    --         match = "Enter passphrase for key",
+    --         type = "secret", -- パスフレーズはシークレットとして扱う
+    --         value_type = "static", -- 毎回新しいパスフレーズを入力するか
+    --         value = "", -- 初期値
+    --     },
+    --     {
+    --         match = "password:",
+    --         type = "secret",
+    --         value_type = "static",
+    --         value = "",
+    --     },
+    --     {
+    --         match = "continue connecting (yes/no/[fingerprint])?",
+    --         type = "plain",
+    --         value_type = "static",
+    --         value = "",
+    --     },
+    -- },
+}
+
 require("lazy").setup({
     defaults = {
         lazy = false,
@@ -189,6 +215,16 @@ require("lazy").setup({
             -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
             -- available after the first executing of it or after a keymap of text-case.nvim has been used.
             lazy = false,
+        },
+        {
+            "amitds1997/remote-nvim.nvim",
+            version = "*", -- Pin to GitHub releases
+            dependencies = {
+                "nvim-lua/plenary.nvim", -- For standard functions
+                "MunifTanjim/nui.nvim", -- To build the plugin UI
+                "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+            },
+            config = true,
         },
         {
             "enoatu/backseat.nvim",
